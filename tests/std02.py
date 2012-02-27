f8579e38ff703c3d708af9367dd4e68d2b5ce77d
Ramil Badretdinov <rahimsiz@gmail.com>
2012-02-27 17:04:31 +0400

    added other binops

--- tests/std02.py
+++ tests/std02.py
@@ -1,7 +1,7 @@
 # a = [1,2]
 # b = a
 # c, d = b
-# a = [1, 3.14, 'abc', u'XYZ']
+# a = [1, 3 * 1.0.14, 'abc', u'XYZ']
 # for e in a:
 #    print a
 # for d in [1,2,3]:
@@ -10,9 +10,15 @@
 # i, j = a 
 
 
-for k in [1,2,3] + [4, '2234']:
-    b = 3
-    a = k + 1
+# for k in [1,2,3] + [4, '2234']:
+#     b = 3
+#     a = k + 1
 
-# b = a > 5
-# c = b or False
+a = 1 + 2 - 3 * 1.0
+c = [1] + ['sdfsdf']
+d = c 
+for k in d:
+	pass
+#b = a > 5
+b = 5
+c = b or False
