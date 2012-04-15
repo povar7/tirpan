ddadd996641baf615602237d958aa61e41bda2e1
Igor Bronshteyn <igor.bronstein@gmail.com>
2012-04-15 19:20:29 +0400

    add several new builtin variables and functions

--- std/builtin.py
+++ std/builtin.py
@@ -9,6 +9,9 @@ from typenodes import *
 type_int  = TypeInt()
 type_type = TypeType()
 
+def quasi_len(scope):
+    return set([type_int])
+
 def quasi_range1(scope):
     type1 = list(scope.findParam(1).nodeType)[0]
     if type1 == type_int:
@@ -32,14 +35,30 @@ def quasi_type_var():
     return type_type
 
 functions = [
+                ['len'  , quasi_len   , 1],                 \
                 ['range', quasi_range1, 1],                 \
                 ['range', quasi_range3, 3, {3 : type_int}], \
                 ['type' , quasi_type1 , 1]                  \
             ]
 
 variables = [                                               \
-                ['type',  quasi_type_var],                  \
-                ['int' ,  quasi_type_var]                   \
+                ['type'     , quasi_type_var],              \
+                ['object'   , quasi_type_var],              \
+                ['int'      , quasi_type_var],              \
+                ['long'     , quasi_type_var],              \
+                ['float'    , quasi_type_var],              \
+                ['bool'     , quasi_type_var],              \
+                ['complex'  , quasi_type_var],              \
+                ['str'      , quasi_type_var],              \
+                ['unicode'  , quasi_type_var],              \
+                ['buffer'   , quasi_type_var],              \
+                ['tuple'    , quasi_type_var],              \
+                ['list'     , quasi_type_var],              \
+                ['dict'     , quasi_type_var],              \
+                ['file'     , quasi_type_var],              \
+                ['xrange'   , quasi_type_var],              \
+                ['slice'    , quasi_type_var],              \
+                ['NameError', quasi_type_var]               \
             ]
 
 modules   = [                                               \
