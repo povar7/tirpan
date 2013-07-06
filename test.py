9410865d01181c49d43ec21f9c03ab511864adab
Igor Bronshteyn <igor.bronstein@gmail.com>
2013-07-06 19:24:47 +0400

    support "object15.py" test case on branch

--- test.py
+++ test.py
@@ -1886,5 +1886,22 @@ class TestObject11(unittest.TestCase):
                         'wrong types calculated')
         self.assertEqual(node.link.name, 'z', 'name is not "z"')
 
+class TestObject15(unittest.TestCase):
+    
+    ast = tirpan.run('tests/object15.py')
+
+    def test_x(self):
+        node = utils.findNode(self.ast, line=17, kind=ast.Name)
+        self.assertTrue(node is not None, 'required node was not found')
+        self.assertTrue(hasattr(node, 'link'), 'node has no link to type info')
+        self.assertTrue(isinstance(node.link, VariableTGNode),
+                        'type is not a var')
+        nodeType = freezeSet(node.link.nodeType)
+        type1 = LiteralSema(float)
+        self.assertTrue(len(nodeType) == 1 and
+                        any(type1 == elem for elem in nodeType),
+                        'wrong types calculated')
+        self.assertEqual(node.link.name, 'x', 'name is not "x"')
+
 if __name__ == '__main__':
     unittest.main()
